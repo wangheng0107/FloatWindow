489e096becec93aa7200a4381d20b3c700521fb8
王恒 <heng.wang@eeoa.com>
2023-03-13 20:25:01 +0800

    移除悬浮窗

--- app/src/main/java/com/example/lc/floatbutton/MainActivity.java
+++ app/src/main/java/com/example/lc/floatbutton/MainActivity.java
@@ -7,21 +7,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import floatmenu.SpeedDialOverlayLayout;
-
 public class MainActivity extends AppCompatActivity {
     FloatWindow dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final SpeedDialOverlayLayout mask=findViewById(R.id.mask);
-        mask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.openOrCloseMenu();
-            }
-        });
+//        final SpeedDialOverlayLayout mask=findViewById(R.id.mask);
+//        mask.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.openOrCloseMenu();
+//            }
+//        });
         dialog=new FloatWindow(this,1,500, new FloatWindow.IOnItemClicked() {
             @Override
             public void onBackItemClick() {
@@ -32,18 +30,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCloseItemClick() {
                 Toast.makeText(MainActivity.this,"关闭",Toast.LENGTH_SHORT).show();
-                mask.hide();
+//                mask.hide();
                 dialog.dismiss();
             }
 
             @Override
             public void onClose() {
-                mask.hide();
+//                mask.hide();
             }
 
             @Override
             public void onExpand() {
-                mask.show();
+//                mask.show();
             }
         });
         Button button=findViewById(R.id.button);
