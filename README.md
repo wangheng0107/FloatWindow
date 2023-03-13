92374e60e26309fb652fe17bacefe7788e12f603
王恒 <heng.wang@eeoa.com>
2023-03-13 17:38:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# FloatWindow
+# 模仿微信浮窗的可折叠自动靠边悬浮按钮  
+FloatWindow 模仿微信浮窗的可折叠自动靠边悬浮按钮，在[FloatMenuSample](https://github.com/crosg/FloatMenuSample)和[FloatingActionButtonSpeedDial](https://github.com/leinardi/FloatingActionButtonSpeedDial)的参考下写的模仿微信浮窗的悬浮自动贴边可展开浮动按钮。
+具体见我的博客：https://blog.csdn.net/qq_31709249/article/details/98449374
+# 效果图：
+![image](https://github.com/MeteorCh/FloatWindow/blob/master/SceenShoot/ScreenreShoot.gif)
+# 更新记录
+* 2019.9.5更新：界面重做，使得界面更加扁平化，更加美观。同时支持设置文字功能，具体见代码。效果如下图所示：
+![image](https://github.com/MeteorCh/FloatWindow/blob/master/SceenShoot/Screenrecorder-2019-09-05-20-53-54-314.gif)
+* 2019.10.13更新：重新设计构造方法，能自定义初始悬浮窗出现在左边还是右边，能自定义悬浮窗初始出现时距离屏幕顶部的距离（即可自定义悬浮窗的位置）。
---
+++ app/src/main/java/com/example/lc/floatbutton/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.lc.floatbutton;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    FloatWindow dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final SpeedDialOverlayLayout mask=findViewById(R.id.mask);
+        mask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.openOrCloseMenu();
+            }
+        });
+        dialog=new FloatWindow(this,1,500, new FloatWindow.IOnItemClicked() {
+            @Override
+            public void onBackItemClick() {
+                Toast.makeText(MainActivity.this,"返回",Toast.LENGTH_SHORT).show();
+                dialog.openOrCloseMenu();
+            }
+
+            @Override
+            public void onCloseItemClick() {
+                Toast.makeText(MainActivity.this,"关闭",Toast.LENGTH_SHORT).show();
+                mask.hide();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onClose() {
+                mask.hide();
+            }
+
+            @Override
+            public void onExpand() {
+                mask.show();
+            }
+        });
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                dialog.show("返回高级查询中的搜索结果");
+            }
+        });
+        Button textColorBtn=(Button)findViewById(R.id.set_text_color);
+        textColorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                dialog.show("返回高级查询中有关<font color='red'>“"+"关键字"+"”</font>的搜索结果");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
