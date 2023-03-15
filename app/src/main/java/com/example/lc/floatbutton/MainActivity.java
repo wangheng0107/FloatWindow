92c9fb47aceff37ffdd03851f1b275b52f642221
王恒 <heng.wang@eeoa.com>
2023-03-15 18:16:47 +0800

    初步完成底部拖拽的需求

--- app/src/main/java/com/example/lc/floatbutton/MainActivity.java
+++ app/src/main/java/com/example/lc/floatbutton/MainActivity.java
@@ -16,12 +16,10 @@ import hover_view.ViewState;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btn)
-    Button mBtn;
-    @BindView(R.id.hv)
-    HoverView mHv;
-    @BindView(R.id.rv)
+    @BindView(R.id.contentView)
     RecyclerView mRv;
+    @BindView(R.id.contentView1)
+    RecyclerView mRv1;
     private MyAdapter mAdapter;
 
     @Override
@@ -30,19 +28,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mHv.getState() == ViewState.CLOSE)  // "关闭" 状态
-                    mHv.changeState(ViewState.HOVER);   // 打开至 "悬停" 状态
-//                    mHv.changeState(ViewState.FILL);   // 打开至 "全屏" 状态
-                else
-                    mHv.changeState(ViewState.CLOSE);   // 切换至 "关闭" 状态
-            }
-        });
-
         mRv.setAdapter(mAdapter = new MyAdapter(this, this));
         mRv.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter.setDataList(Arrays.asList(
+                "1", "2", "3", "4", "5",
+                "6", "7", "8", "9", "10",
+                "11", "12", "13", "14", "15",
+                "16", "17", "18", "19", "20"
+        ));
+        mRv1.setAdapter(mAdapter = new MyAdapter(this, this));
+        mRv1.setLayoutManager(new LinearLayoutManager(this));
 
         mAdapter.setDataList(Arrays.asList(
                 "1", "2", "3", "4", "5",
